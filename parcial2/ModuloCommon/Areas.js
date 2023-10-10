e8f63afc38c1764597bc0c195712d51ff58fe7b3
Jaqueline Cruz <jackcrzob20@gmail.com>
2023-10-10 07:56:50 -0500

    modificaciones/mejoras

--- parcial2/ModuloCommon/Areas.js
+++ parcial2/ModuloCommon/Areas.js
@@ -5,8 +5,8 @@ function areaTrapecio(baseMayor, baseMenor, altura){
     return (((baseMayor+baseMenor)/2)*altura);
 }
 //modulo-->archivo que tiene funciones y las exporta
-module.exports=areaRectangulo=areaRectangulo;
-module.exports=areaTrapecio=areaTrapecio;
+module.exports.areaRectangulo=areaRectangulo;
+module.exports.areaTrapecio=areaTrapecio;
 
 // console.log(__dirname);
 // //archivo donde esta abierto
